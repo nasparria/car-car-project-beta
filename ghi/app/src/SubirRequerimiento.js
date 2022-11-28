46399d613e5d718c9273b8bdaa19bda022e0423c
nicolas asparria <nasparria@gmail.com>
2022-11-28 18:55:05 -0500

    correctly populated tipos (options) to the frontend

--- ghi/app/src/SubirRequerimiento.js
+++ ghi/app/src/SubirRequerimiento.js
@@ -1,164 +1,161 @@
-// import React from 'react'
+import React from 'react'
 
-// class SubirRequerimientoForm extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         // let tipoRequerimiento = [
-//         //     { label: "tecnologia", value: "TECNOLOGIA" },
-//         //     { label: "marketingDigital", value: "MKT" },
-//         //     { label: "analytics", value: "ANALYTICIS" }
-//         // ]
-
-//         // let [fruit, setTipo] = useState("Seleccciona un tipo de requerimiento")
-//         // let handleTiposChange = (e) => {
-//         //     setTipo(e.target.value)
-//         //   }
-
-//         // TIPO_REQUIRIMIENTO = (
-//         //     ('tecnologia','TECNOLOGIA'),
-//         //     ('marketing digital', 'MKT'),
-//         //     ('analytics','ANALYTICIS'),
-//         // )
-//         this.state = {
-//             empresa: '',
-//             titulo: '',
-//             descripcion: '',
-//             enlace: '',
-//             date: '',
-//             time: '',
-//             tipo: '',
-//             tipos: [],
-//         }
-//         this.handleEmpresaChange = this.handleEmpresaChange.bind(this)
-//         this.handleTituloNameChange = this.handleTituloNameChange.bind(this)
-//         this.handleEnlaceChange = this.handleEnlaceChange.bind(this)
-//         this.handleDateChange = this.handleDateChange.bind(this)
-//         this.handleTimeChange = this.handleTimeChange.bind(this)
-//         this.handleTipoChange = this.handleTipoChange.bind(this)
-//         this.handleSubmit = this.handleSubmit.bind(this)
-//     }
+class SubirRequerimientoForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            empresa: '',
+            titulo: '',
+            descripcion: '',
+            enlace: '',
+            date: '',
+            time: '',
+            tipo: '',
+            tipos: [],
+        }
+        this.handleEmpresaChange = this.handleEmpresaChange.bind(this)
+        this.handleTituloNameChange = this.handleTituloNameChange.bind(this)
+        this.handleEnlaceChange = this.handleEnlaceChange.bind(this)
+        this.handleDateChange = this.handleDateChange.bind(this)
+        this.handleTimeChange = this.handleTimeChange.bind(this)
+        this.handleTipoChange = this.handleTipoChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
 
 
-//     async handleSubmit(event) {
-//         event.preventDefault();
-//         const datos = { ...this.state };
-//         // delete datos.technicians;
+    async componentDidMount() {
+        //const statesHost = `${process.env.REACT_APP_ACCOUNT_API}`
+        const statesHost = 'http://localhost:8080'
+        const URL = statesHost + '/tipos/';
+        const response = await fetch(URL);
+        if (response.ok) {
+            const data = await response.json();
+            this.setState({ tipos: data.tipos });
+        }
+    }
 
-//         const requerimientosUrl = 'http://localhost:8080/requerimientos/';
-//         const fetchConfig = {
-//             method: "post",
-//             body: JSON.stringify(datos),
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//         };
-//         const response = await fetch(requerimientosUrl, fetchConfig);
-//         if (response.ok) {
-//             const newRequerimiento = await response.json();
+    async handleSubmit(event) {
+        event.preventDefault();
+        const data = { ...this.state };
+        delete data.tipos;
 
-//             const cleared = {
-//                 empresa: '',
-//                 titulo: '',
-//                 descripcion: '',
-//                 enlace: '',
-//                 date: '',
-//                 time: '',
-//                 tipo: '',
-//             };
-//             this.setState(cleared);
-//         }
-//     }
+        const requerimientosHost = 'http://localhost:8080'
+        const url = requerimientosHost + `/requerimientos/`;
+        const fetchConfig = {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include'
+        };
+        const response = await fetch(url, fetchConfig);
+        if (response.ok) {
+            let successTag = document.getElementById('success-message');
+            let formTag = document.getElementById('create-owner-form');
+            successTag.classList.remove('d-none');
+            formTag.classList.add('d-none');
+            this.setState({
+                empresa: '',
+                titulo: '',
+                descripcion: '',
+                enlace: '',
+                date: '',
+                time: '',
+                tipo: '',
+            });
+        }
+    }
 
-//     handleEmpresaChange(event) {
-//         const value = event.target.value;
-//         this.setState({ empresa: value })
-//     }
+    handleEmpresaChange(event) {
+        const value = event.target.value;
+        this.setState({ empresa: value })
+    }
 
-//     handleDateChange(event) {
-//         const value = event.target.value;
-//         this.setState({ date: value })
-//     }
+    handleDateChange(event) {
+        const value = event.target.value;
+        this.setState({ date: value })
+    }
 
-//     handleTituloNameChange(event) {
-//         const value = event.target.value;
-//         this.setState({ titulo: value })
-//     }
+    handleTituloNameChange(event) {
+        const value = event.target.value;
+        this.setState({ titulo: value })
+    }
 
-//     handleTimeChange(event) {
-//         const value = event.target.value;
-//         this.setState({ time: value })
-//     }
+    handleTimeChange(event) {
+        const value = event.target.value;
+        this.setState({ time: value })
+    }
 
-//     handleEnlaceChange(event) {
-//         const value = event.target.value;
-//         this.setState({ enlace: value })
-//     }
+    handleEnlaceChange(event) {
+        const value = event.target.value;
+        this.setState({ enlace: value })
+    }
 
-//     handleTipoChange(event) {
-//         const value = event.target.value;
-//         this.setState({ tipo: value })
-//     }
+    handleTipoChange(event) {
+        const value = event.target.value;
+        this.setState({ tipo: value })
+    }
 
-//     // async componentDidMount() {
-//     //     const url = 'http://localhost:8080/technicians/';
+    // async componentDidMount() {
+    //     const url = 'http://localhost:8080/technicians/';
 
-//     //     const response = await fetch(url);
+    //     const response = await fetch(url);
 
-//     //     if (response.ok) {
-//     //         const data = await response.json();
+    //     if (response.ok) {
+    //         const data = await response.json();
 
-//     //         this.setState({ technicians: data.technicians });
+    //         this.setState({ technicians: data.technicians });
 
-//     //     }
-//     // }
+    //     }
+    // }
 
-//     render() {
-//         return (
-//             <div className="row">
-//                 <div className="offset-3 col-6">
-//                     <div className="shadow p-4 mt-4">
-//                         <h1>Crear un nuevo requerimiento!</h1>
-//                         <form onSubmit={this.handleSubmit} id="create-service-form">
-//                             <div className="form-floating mb-3">
-//                                 <input onChange={this.handleEmpresaChange} value={this.state.empresa} placeholder="Empresa" required type="text" name="empresa" id="empresa" className="form-control" />
-//                                 <label htmlFor="empresa">Empresa</label>
-//                             </div>
-//                             <div className="form-floating mb-3">
-//                                 <input onChange={this.handleTituloNameChange} value={this.state.titulo} placeholder="Titulo" required type="text" name="titulo" id="titulo" className="form-control" />
-//                                 <label htmlFor="titulo">Titulo</label>
-//                             </div>
-//                             <div className="form-floating mb-3">
-//                                 <input onChange={this.handleDateChange} value={this.state.date} placeholder="Date" required type="date" name="date" id="date" className="form-control" />
-//                                 <label htmlFor="date">Date</label>
-//                             </div>
-//                             <div className="form-floating mb-3">
-//                                 <input onChange={this.handleTimeChange} value={this.state.time} placeholder="Time" required type="time" name="time" id="time" className="form-control" />
-//                                 <label htmlFor="time">Time</label>
-//                             </div>
-//                             <div className="form-floating mb-3">
-//                                 <input onChange={this.handleEnlaceChange} value={this.state.enlace} placeholder="Enlace" required type="text" name="enlace" id="enlace" className="form-control" />
-//                                 <label htmlFor="enlace">Enlace</label>
-//                             </div>
-//                             <div className="mb-3">
-//                             <select onChange={this.handleTipoChange} value={this.state.tipo} required name="tipo" id="tipo" className="form-select">
-//                                             <option value="">Choose Your State</option>
-//                                             {this.tipo.tipos.map((tipo) => {
-//                                                 return (
-//                                                     <option key={tipo.id} value={state.abbreviation}>
-//                                                         {state.name}
-//                                                     </option>
-//                                                 )
-//                                             })}
-//                                         </select>
+    render() {
+        return (
+            <div className="row">
+                <div className="offset-3 col-6">
+                    <div className="shadow p-4 mt-4">
+                        <h1>Crear un nuevo requerimiento!</h1>
+                        <form onSubmit={this.handleSubmit} id="create-service-form">
+                            <div className="form-floating mb-3">
+                                <input onChange={this.handleEmpresaChange} value={this.state.empresa} placeholder="Empresa" required type="text" name="empresa" id="empresa" className="form-control" />
+                                <label htmlFor="empresa">Empresa</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input onChange={this.handleTituloNameChange} value={this.state.titulo} placeholder="Titulo" required type="text" name="titulo" id="titulo" className="form-control" />
+                                <label htmlFor="titulo">Titulo</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input onChange={this.handleDateChange} value={this.state.date} placeholder="Date" required type="date" name="date" id="date" className="form-control" />
+                                <label htmlFor="date">Date</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input onChange={this.handleTimeChange} value={this.state.time} placeholder="Time" required type="time" name="time" id="time" className="form-control" />
+                                <label htmlFor="time">Time</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input onChange={this.handleEnlaceChange} value={this.state.enlace} placeholder="Enlace" required type="text" name="enlace" id="enlace" className="form-control" />
+                                <label htmlFor="enlace">Enlace</label>
+                            </div>
+                            <div className="mb-3">
+                                        <select onChange={this.handleTipoChange} value={this.state.tipo} required name="tipo" id="tipo" className="form-select">
+                                            <option value="">Choose Your Tipo</option>
+                                            {this.state.tipos.map((tipo) => {
+                                                return (
+                                                    <option key={tipo.id} value={tipo.abbreviation}>
+                                                        {tipo.name}
+                                                    </option>
+                                                )
+                                            })}
+                                        </select>
+                                    </div>
+                            <button className="btn btn-primary">Crear un requerimiento!</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
-//                             </div>
-//                             <button className="btn btn-primary">Crear un requerimiento!</button>
-//                         </form>
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
-// export default SubirRequerimientoForm;
+export default SubirRequerimientoForm;
